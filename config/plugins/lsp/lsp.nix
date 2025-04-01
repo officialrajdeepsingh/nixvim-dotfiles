ef08c85eb6a9228418396d853640f115732c8fce
Rajdeep Singh <radewala.2@gmail.com>
2025-04-01 19:14:10 +0530

    fix the format issue

--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -1,4 +1,3 @@
-{ ... }:
 {
   plugins = {
     lsp-lines = {
@@ -54,26 +53,26 @@
         };
         yamlls = {
           enable = true;
-          extraOptions = {
-            settings = {
-              yaml = {
-                schemas = {
-                  kubernetes = "'*.yaml";
-                  "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
-                  "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
-                  "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
-                  "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
-                  "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
-                  "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
-                  "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
-                  "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" =
-                    "*docker-compose*.{yml,yaml}";
-                  "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" =
-                    "*flow*.{yml,yaml}";
-                };
-              };
-            };
-          };
+          # extraOptions = {
+          # settings = {
+          #    yaml = {
+          #      schemas = {
+          #        kubernetes = "'*.yaml";
+          #        "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
+          #        "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
+          #        "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
+          #        "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
+          #        "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
+          #        "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
+          #        "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
+          #        "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" =
+          #          "*docker-compose*.{yml,yaml}";
+          #        "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" =
+          #          "*flow*.{yml,yaml}";
+          #      };
+          #    };
+          #  };
+          # };
         };
 
       };
